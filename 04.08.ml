bcdd00d3207a4bf853fef2f3b85639e9f8ab7aca
Takafumi <t.sekiguchi@sproutatwork.com>
2018-05-24 08:43:10 -0400

    Fix typo

--- 04.08.ml
+++ 04.08.ml
@@ -1,7 +1,7 @@
 (* To return how many swans there are when the sum of swans and tortles, and their legs are given *)
 (* tsurukame : int -> int -> int*)
 
-let tsurukame Heads legs = (4 * heads - legs) / 2
+let tsurukame heads legs = (4 * heads - legs) / 2
 
 let test1 = tsurukame 1 2 = 1
 let test1 = tsurukame 1 4 = 0
